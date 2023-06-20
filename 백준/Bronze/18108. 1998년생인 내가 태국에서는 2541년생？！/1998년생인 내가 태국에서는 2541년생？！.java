7f7c1fdd3b4a16053e2bfc657928399c5b42b803
taebak2 <133829003+taebak2@users.noreply.github.com>
2023-06-20 22:50:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 224 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int taeGook = s.nextInt();
+        int korea = taeGook-543;
+        System.out.println(korea);
+    }
+}
