a19261e6b9f1dec9c34067251c742e456a78f855
taebak2 <133829003+taebak2@users.noreply.github.com>
2023-06-22 00:30:34 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        {
+            Scanner s = new Scanner(System.in);
+            System.out.println(s.nextLong() + s.nextLong() + s.nextLong());
+        }
+
+
+    }
+
+}
