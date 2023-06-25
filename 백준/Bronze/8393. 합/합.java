40531f782764e605d22a281072caa5d3d796c066
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-06-25 21:27:03 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int num = s.nextInt();
+
+
+        System.out.println(num*(num+1)/2);
+
+    }
+}
