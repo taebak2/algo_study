ffd21cda054cadc613d9923ef5e827b70c1fac7f
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-06-25 20:59:05 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18460 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt();
+
+        while (true) {
+            System.out.println(N + " * 1 = " + N * 1);
+            System.out.println(N + " * 2 = " + N * 2);
+            System.out.println(N + " * 3 = " + N * 3);
+            System.out.println(N + " * 4 = " + N * 4);
+            System.out.println(N + " * 5 = " + N * 5);
+            System.out.println(N + " * 6 = " + N * 6);
+            System.out.println(N + " * 7 = " + N * 7);
+            System.out.println(N + " * 8 = " + N * 8);
+            System.out.println(N + " * 9 = " + N * 9);
+        break;}
+
+
+    }
+
+}
