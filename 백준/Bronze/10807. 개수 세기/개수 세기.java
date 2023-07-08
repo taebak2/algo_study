f4117564730a3f078c529d4b59d6d02c5ba5f0ef
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-08 17:34:01 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int []arr = new int[num]; // 배열 지정
+        for (int i = 0; i < num; i++) {
+                arr[i] = s.nextInt();
+        }
+        int count=0;
+        int num1 = s.nextInt();
+        for (int i = 0; i < num; i++) {
+            if(arr[i]==num1){
+                count++;
+
+            }
+        }
+        System.out.println(count);
+    }
+}
+
