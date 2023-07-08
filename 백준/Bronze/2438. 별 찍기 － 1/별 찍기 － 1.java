17b46e576128a01271665dec6cdd7ae5060a607c
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-08 15:25:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 308 ms, Memory: 18052 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int star = s.nextInt(); // int 선언
+        for (int i = 0; i < star; i++) { // 총 5줄
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
+
