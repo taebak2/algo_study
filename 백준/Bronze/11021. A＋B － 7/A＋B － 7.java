5acdb5ce5ae158bbd292800a2d6544ead7be606c
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-08 15:03:41 +0900

    [Bronze V] Title: A+B - 7, Time: 272 ms, Memory: 18944 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int result = s.nextInt();
+        for (int i = 1; i <= result; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            String c = "Case #" + i;
+            System.out.println(c+ ": " + (a + b));
+        }
+
+    }
+
+}
