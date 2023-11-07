34a5d28f3114f2f5a4ca345935fd94d03aa34356
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-11-07 22:28:02 +0900

    [Bronze III] Title: 최소, 최대, Time: 1680 ms, Memory: 317520 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int num[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			num[i] = s.nextInt();
+		}
+
+		int min = num[0]; // 초기값을 배열의 첫 번째 요소로 설정
+		int max = num[0]; // 초기값을 배열의 첫 번째 요소로 설정
+
+		// 배열을 순회하면서 최소값과 최대값을 찾음
+		for (int i = 1; i < n; i++) {
+			if (num[i] < min) {
+				min = num[i];
+			}
+			if (num[i] > max) {
+				max = num[i];
+			}
+		}
+
+		System.out.println(min + " " + max);
+	}
+}
