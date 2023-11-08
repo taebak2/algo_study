7de404b03b636a25a3c6dc3f6dcbdb5e4e045d2d
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-11-08 23:33:49 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int arr[] = { s.nextInt(), s.nextInt(), s.nextInt(), s.nextInt(), s.nextInt(), s.nextInt(), s.nextInt(),
+				s.nextInt(), s.nextInt() };
+
+		int count = 0;
+		int max = 0;
+		int index = 0;
+
+		for (int value : arr) {
+			count++;
+			if (value > max) {
+				max = value;
+				index = count;
+
+			}
+		}
+		System.out.println(max);
+		System.out.println(index);
+	}
+
+}
