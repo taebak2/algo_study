46d50b04ac298ada5da66b38261276f872290f0b
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-11-10 00:20:09 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        boolean[] isAttended = new boolean[31]; // 1부터 30까지의 학생 번호를 나타내는 배열
+
+        for (int i = 0; i < 28; i++) {
+            int studentNumber = s.nextInt();
+            isAttended[studentNumber] = true;
+        }
+
+        for (int i = 1; i <= 30; i++) {
+            if (!isAttended[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
