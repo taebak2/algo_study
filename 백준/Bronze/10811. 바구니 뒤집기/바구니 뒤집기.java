eb65f1c714ca26cfb6b0d76fc9c5fc5d80771928
Kim-taegyun <xorbs3902@gmail.com>
2023-11-14 22:06:15 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 232 ms, Memory: 18072 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		Integer arr[] = new Integer[s.nextInt()];
+		int order = s.nextInt();
+		int temp = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1; // 0 = 1, 1 = 2, 2= = 3...
+
+		}
+
+		for (int i = 0; i < order; i++) {
+			int min = s.nextInt() - 1;
+			int max = s.nextInt() - 1;
+
+			while (min < max) {
+				temp = arr[min];
+				arr[min] = arr[max];
+				arr[max] = temp;
+				min++;
+				max--;
+
+			}
+
+		}
+		for (Integer integer : arr) {
+			System.out.print(integer + " ");
+		}
+
+	}
+
+}
