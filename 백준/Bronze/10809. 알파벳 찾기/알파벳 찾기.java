10fa0ba4a305527d18d148b1b05521778a90f807
Kim-taegyun <xorbs3902@gmail.com>
2023-11-15 17:08:21 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		String word = s.next(); // 단어
+		String place[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+				"s", "t", "u", "v", "w", "x", "y", "z" };
+		for (int i = 0; i < place.length; i++) {
+			System.out.print(word.indexOf(place[i]) + " ");
+
+		}
+
+	}
+}
