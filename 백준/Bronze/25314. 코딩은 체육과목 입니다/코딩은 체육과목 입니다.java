9d9beb85fd68584dec2311230352dd4a341da6a3
Kim-taegyun <xorbs3902@gmail.com>
2024-02-14 13:14:43 +0900

    Update 코딩은 체육과목 입니다.java

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         if (num % 4 == 0) {
-            for (int i = 0; i < num / 4; i++) {
+            for (int i = 0; i < num / 4; i++) { 
 
                  System.out.print("long ");
             }
