57be53429c1bcf7a8a636ed1005b323e0e4367cc
Kim-taegyun <xorbs3902@gmail.com>
2024-02-17 07:11:05 +0900

    Update 나머지.java

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		Scanner s = new Scanner(System.in);
 		int arr[] = new int[10];
 		for (int i = 0; i < arr.length; i++) {
