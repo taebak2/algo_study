37b87c488727fba23323b014cf6cb68d384d5faa
Kim-taegyun <xorbs3902@gmail.com>
2024-02-24 01:20:25 +0900

    Update 직사각형.java

--- 백준/Bronze/27323. 직사각형/직사각형.java
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        
+         
     
        int A = s.nextInt();
        int B = s.nextInt();
