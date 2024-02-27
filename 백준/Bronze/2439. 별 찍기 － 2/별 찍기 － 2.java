47c87eefd4899b6895f8093962200793c523ca42
Kim-taegyun <xorbs3902@gmail.com>
2024-02-28 00:56:55 +0900

    Update 별 찍기 － 2.java

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int star = s.nextInt(); // int 선언
-        for (int i = 0; i < star; i++) { // 총 5줄
+        for (int i = 0; i < star; i++) { // 총 5줄 
             for (int j = star; j-1>i; j--) {
                 System.out.print(" ");
             }
