068dce5a4473d7da03203df6dbbd7a7de5f8470e
Kim-taegyun <xorbs3902@gmail.com>
2024-03-02 01:32:37 +0900

    Update 1998년생인 내가 태국에서는 2541년생？！.java

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int taeGook = s.nextInt();
-        int korea = taeGook-543;
+        int korea = taeGook-543; 
         System.out.println(korea);
     }
 }
