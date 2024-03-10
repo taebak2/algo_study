9b112bc76e50dee79b557c976bca2701607dd71a
Kim-taegyun <xorbs3902@gmail.com>
2024-03-11 08:41:17 +0900

    Update 시험 성적.java

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        int score = s.nextInt();
+        int score = s.nextInt(); 
 
         if (score >= 90 & score <= 100) {
             System.out.println("A");
