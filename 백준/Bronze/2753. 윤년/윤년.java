d950fada949f5215eab20fb421d0e4a14e62662b
Kim-taegyun <xorbs3902@gmail.com>
2024-03-12 09:21:56 +0900

    Update 윤년.java

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -10,7 +10,7 @@ public class Main {
         int year = s.nextInt();
 
         if (year % 4 == 0) {
-            if (year % 400 == 0) {
+            if (year % 400 == 0) { 
                 System.out.println("1");
             } else if (year % 100 == 0) {
                 System.out.println("0");
