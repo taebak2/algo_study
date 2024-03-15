cde5b925fb59b37c8fd8b687fdb260158b4f5660
Kim-taegyun <xorbs3902@gmail.com>
2024-03-15 15:32:47 +0900

    Update 구구단.java

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -14,7 +14,7 @@ public class Main {
             System.out.println(N + " * 3 = " + N * 3);
             System.out.println(N + " * 4 = " + N * 4);
             System.out.println(N + " * 5 = " + N * 5);
-            System.out.println(N + " * 6 = " + N * 6);
+            System.out.println(N + " * 6 = " + N * 6); 
             System.out.println(N + " * 7 = " + N * 7);
             System.out.println(N + " * 8 = " + N * 8);
             System.out.println(N + " * 9 = " + N * 9);
