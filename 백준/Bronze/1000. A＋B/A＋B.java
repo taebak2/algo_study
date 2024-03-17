7a37e03e630d92185ddf3c43dc22e97fccb6c401
Kim-taegyun <xorbs3902@gmail.com>
2024-03-18 08:42:18 +0900

    Update A＋B.java

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args){
            Scanner s = new Scanner(System.in);
         int A = s.nextInt();
-        int B = s.nextInt();
+        int B = s.nextInt(); 
         System.out.println(A+B);
     }
 }
