8b4df7f8512082c746119dbcfd52b643dc527d05
Kim-taegyun <xorbs3902@gmail.com>
2024-03-19 01:10:37 +0900

    Update 삼각형 외우기.java

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 
-		int a = s.nextInt();
+		int a = s.nextInt(); 
 		int b = s.nextInt();
 		int c = s.nextInt();
 
