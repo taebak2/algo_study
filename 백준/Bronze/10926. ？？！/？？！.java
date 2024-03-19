e9b6c1ca826364956dbc5c69ca3c0859addc4b55
Kim-taegyun <xorbs3902@gmail.com>
2024-03-20 00:04:16 +0900

    Update ？？！.java

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String name = s.next();
-        System.out.println(name+"??!");
+        System.out.println(name+"??!"); 
 
     }
 }
