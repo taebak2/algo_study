6c16c101b322e178afa0968190628c40a11b6420
Kim-taegyun <xorbs3902@gmail.com>
2024-03-29 00:07:59 +0900

    Update 꼬마 정민.java

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         {
-            Scanner s = new Scanner(System.in);
+            Scanner s = new Scanner(System.in); 
             System.out.println(s.nextLong() + s.nextLong() + s.nextLong());
         }
 
