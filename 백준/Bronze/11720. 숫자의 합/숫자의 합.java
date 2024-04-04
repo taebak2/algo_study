5adb010997de942c6210bf27c9539eaa473d83eb
Kim-taegyun <xorbs3902@gmail.com>
2024-04-05 00:13:56 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -8,7 +8,7 @@ public class Main {
         String sn = s.next();
         s.close();
  
-        int sum = 0;
+        int sum = 0; 
         for (int i = 0; i < num; i++) {
             sum+=sn.charAt(i) - 48;
         }
