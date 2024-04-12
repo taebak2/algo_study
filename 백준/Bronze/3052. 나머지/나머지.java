6e84af83cd62213fe536df01e8b5d4cea63b8b9b
Kim-taegyun <xorbs3902@gmail.com>
2024-04-13 00:15:16 +0900

    Update 나머지.java

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -7,7 +7,7 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		int arr[] = new int[10];
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = s.nextInt(); // 10개의 숫자를 입력해
+			arr[i] = s.nextInt(); // 10개의 숫자를 입력해 
 			arr[i] = arr[i] % 42; // 42로 나눈 나머지값
 		}
 		arr = Arrays.stream(arr).distinct().toArray();
