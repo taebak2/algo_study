fff2f45205816fb0737b136570ad65c551d5dcc0
Kim-taegyun <xorbs3902@gmail.com>
2024-04-23 16:59:06 +0900

    Update 알파벳 찾기.java

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -6,7 +6,7 @@ public class Main {
 		Scanner s = new Scanner(System.in);
  
 		String word = s.next(); // 단어   
-		String place[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+		String place[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 
 				"s", "t", "u", "v", "w", "x", "y", "z" };
 		for (int i = 0; i < place.length; i++) { 
 			System.out.print(word.indexOf(place[i]) + " ");
