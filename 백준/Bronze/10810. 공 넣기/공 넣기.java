70a3f11afc8aa95b3272b4a911b6522dcb9a4e0a
Kim-taegyun <xorbs3902@gmail.com>
2024-04-24 12:54:34 +0900

    Update 공 넣기.java

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -13,7 +13,7 @@ public class Main {
 		int b = 0; 								  
 		int c = 0; 								
  
-		for (int i = 0; i < ball; i++) { 		
+		for (int i = 0; i < ball; i++) { 		 
 			a = s.nextInt(); 					 
 			b = s.nextInt(); 					
 			c = s.nextInt(); 					
